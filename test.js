c5e6026557a00baff969a2e2de5adc407049c978
Pathompat <mungonepiece@gmail.com>
2017-09-11 18:13:34 +0700

    init2

--- test.js
+++ test.js
@@ -2,5 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, 'ocpp-server-test.herokuapp.com');
-console.log('Server running at http://ocpp-server-test.herokuapp.com:8080/');
+}).listen(process.env.PORT);
