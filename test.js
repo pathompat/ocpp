17c04440b717ba80312cc405dac4a4664828226a
Pathompat <mungonepiece@gmail.com>
2017-09-11 17:51:26 +0700

    Debug2

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, 'https://ocpp-server-test.herokuapp.com/');
-//console.log('Server running at http://127.0.0.1:1337/');
+}).listen(8080, 'ocpp-server-test.herokuapp.com');
+console.log('Server running at http://ocpp-server-test.herokuapp.com:8080/');
